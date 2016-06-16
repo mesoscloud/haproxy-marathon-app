65c047b9889d454c29009dfd8378852511fad1ef
Peter Ericson <pdericson@gmail.com>
2016-06-17 00:29:09 +1000

    Stats enable on port 9999

--- app.py
+++ app.py
@@ -1,31 +1,16 @@
-#!/usr/bin/python -u
-#
-# Copyright (c) 2015 Peter Ericson <pdericson@gmail.com>
-#
-# Permission to use, copy, modify, and distribute this software for any
-# purpose with or without fee is hereby granted, provided that the above
-# copyright notice and this permission notice appear in all copies.
-#
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
-# WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
-# MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
-# ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
-# WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
-# ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
-# OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+#!/usr/bin/env python
 
 """Generate HAProxy configuration (using Marathon as a data source)"""
 
-import json
 import logging
 import os
 import re
-import subprocess
 import sys
 import time
 
 import jinja2
 import kazoo.client
+import requests
 
 
 def main():
@@ -50,16 +35,16 @@ def main():
     while True:
         apps = []
 
-        p = subprocess.Popen(["curl", "-fsS", "http://%s/v2/apps" % marathon], stdout=subprocess.PIPE)
-        assert p.wait() == 0
+        r = requests.get("http://%s/v2/apps" % marathon)
+        r.raise_for_status()
 
-        appIds = [app['id'] for app in json.loads(p.stdout.read().decode('utf-8'))['apps']]
+        appIds = [app['id'] for app in r.json()['apps']]
 
         for appId in appIds:
-            p = subprocess.Popen(["curl", "-fsS", "http://%s/v2/apps%s" % (marathon, appId)], stdout=subprocess.PIPE)
-            assert p.wait() == 0
+            r = requests.get("http://%s/v2/apps%s" % (marathon, appId))
+            r.raise_for_status()
 
-            app = json.loads(p.stdout.read().decode('utf-8'))['app']
+            app = r.json()['app']
 
             if 'portMappings' not in app['container']['docker']:
                 continue
